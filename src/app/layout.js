bcbc0ec8d3c645961ea23b5d0a2e0d8b1da7b9bb
SubaruDev0 <subaru0.dev@gmail.com>
2026-07-28 14:31:28 -0400

    mejora complementaria burger

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,11 +4,17 @@ import { ClientShell } from '../components/ClientShell';
 export const metadata = {
   title: 'Vega',
   description: 'Gestion de pedidos para puesto de feria',
+  icons: {
+    icon: '/favicon.svg',
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
       <body>
         <ClientShell>{children}</ClientShell>
       </body>
